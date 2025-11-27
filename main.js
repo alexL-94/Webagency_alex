c2417b789f2077b273d8cf45469d2735f9cef99d
Alexander <alexander.aguirre@live.se>
2025-11-27 22:12:21 +0100

    klar med de mesta av sidan

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
+/*  Första scroll animation på index.html för div1 och div2 */
+
 document.addEventListener('DOMContentLoaded', () => {
     const divs = document.querySelectorAll('.div1, .div2');
 
+
+    /*  Skapar en intersectionObserver som lyssnar på när elementet blir synligt i webbläsaren */
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
@@ -13,10 +17,11 @@ document.addEventListener('DOMContentLoaded', () => {
         threshold: 0,
     });
 
+    /*  triggar så fort elementet blir synligt lite */
     divs.forEach(div => observer.observe(div))
 });
 
-
+/*  Andra scroll för about.html gäller office & work */
 document.addEventListener('DOMContentLoaded', () => {
 
     const items = document.querySelectorAll('.office1, .office2, .work-with1, .work-with2');
@@ -28,6 +33,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 observer.unobserve(entry.target);
             }
         });
+
+        /*  triggar när 10% syns av elementet */
     }, { threshold: 0.1 });
 
     items.forEach(item => observer.observe(item));
