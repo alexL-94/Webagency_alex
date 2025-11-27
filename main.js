975b3fa0aca21e9b91a0b7d8dd439cf5b8190c17
Alexander <alexander.aguirre@live.se>
2025-11-27 21:55:02 +0100

    sidan är nästan färdig

--- main.js
+++ main.js
@@ -15,3 +15,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
     divs.forEach(div => observer.observe(div))
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const items = document.querySelectorAll('.office1, .office2, .work-with1, .work-with2');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.1 });
+
+    items.forEach(item => observer.observe(item));
+});
